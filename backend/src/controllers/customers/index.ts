60ddb8630b6f9ad232e66dcad3ca4e98196f80e4
Thais-Gonzaga <devthaisgonzaga@gmail.com>
2024-03-18 16:05:17 -0300

    refactor: add try catch in controller

--- backend/src/controllers/customers/index.ts
+++ backend/src/controllers/customers/index.ts
@@ -19,20 +19,34 @@ class CustomersController {
     }
   };
 
-  public getAll = async (_req: Request, res: Response) => {
-    const customers = await this.customersService.getAll();
-    res.status(statusCodes.OK).json(customers);
+  public getAll = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const customers = await this.customersService.getAll();
+      res.status(statusCodes.OK).json(customers);
+    } catch (error) {
+      next(error);
+    }
   };
 
-  public getId = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const customer = await this.customersService.getId(id);
-    res.status(statusCodes.OK).json(customer);
+  public getId = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      if (!id) throw new Error();
+      const customer = await this.customersService.getId(id);
+      res.status(statusCodes.OK).json(customer);
+    } catch (error) {
+      next(error);
+    }
   };
-  public update = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const customer = await this.customersService.update(id, req.body);
-    res.status(statusCodes.OK).json(customer);
+
+  public update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const customer = await this.customersService.update(id, req.body);
+      res.status(statusCodes.OK).json(customer);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
