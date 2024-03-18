1428d93eb794f31ffb2f59372609c15f007c5ba0
Thais-Gonzaga <devthaisgonzaga@gmail.com>
2024-03-18 02:35:35 -0300

    refactor: adjusts modifications to created components

--- frontend/src/pages/customersList/index.tsx
+++ frontend/src/pages/customersList/index.tsx
@@ -5,25 +5,40 @@ import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { fetcher } from '@/services/backend/fetcher';
 import { Customers } from '@/types';
+import { Link } from 'react-router-dom';
 import useSWR from 'swr';
 
 function CustomersList() {
   const { data = [] } = useSWR<Customers[]>('/customers', fetcher);
   console.log(data);
   return (
-    <div className="flex-col min-w-4xl ">
+    <div className="flex-col w-[90%] max-[663px]:w-full m-auto">
       <HeaderCustomers />
-      <div className="p-6 mx-auto  space-y-4">
-        {data.map(({ name, individualTaxpayer, email, phone, status }) => (
+      <div className="flex px-8 mx-auto mt-16 justify-between">
+        <CustomersData
+          className="w-auto"
+          text="Listagem de usuários"
+          subText="Escolha um cliente para visualizar os detalhes"
+        />
+        <div className="pr-6 lg:pr-48">
+          <Button asChild variant="secondary" size="sm">
+            <Link to="/customers">Novo cliente</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="p-6 mx-auto space-y-4">
+        {data.map(({ name, individualTaxpayer, email, phone, status, id }) => (
           <Card
-            key={individualTaxpayer}
-            className="flex justify-around items-center"
+            key={id}
+            className="flex justify-around items-center min-h-20 max-[663px]:flex-col max-[663px]:p-3"
           >
             <CustomersData text={name} subText={email} />
             <CustomersData text={individualTaxpayer} subText={phone} />
             <CustomersStatus status={status} />
-            <div>
-              <Button variant="outline">Editar</Button>
+            <div className="flex w-[20%] lg:w-[15%]">
+              <Button asChild variant="outline" size="lg">
+                <Link to={`/customers?edit=true&id=${id}`}>Editar</Link>
+              </Button>
             </div>
           </Card>
         ))}
